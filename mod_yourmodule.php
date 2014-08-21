7b6b779eee817054a11a0b0c06ff78f0da178a42
Ove Eriksson <tobby.eriksson@gmail.com>
2014-08-21 13:37:54 +0200

    Create

---
+++ mod_yourmodule.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * package     Your module
+ * copyright   Copyright (C) 2014 Joomla Doe
+ * license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die( 'Restricted access' );
+
+require JModuleHelper::getLayoutPath('mod_yourmodule', $params->get('layout', 'default'));
