0608f54993c7cd909fb899cd42b7697d7dc6d6b1
Ove Eriksson <tobby.eriksson@gmail.com>
2014-08-21 13:25:30 +0200

    create

---
+++ media/js/yourmodule.js
@@ -0,0 +1,9 @@
+/**
+ * package     Your module
+ * copyright   Copyright (C) 2014 Joomla Doe
+ * license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+jQuery( document ).ready(function()
+		{
+			alert('I am an alert from yourmodule.js!');});
