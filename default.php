b99bbd1293d1d31b634c0bcc7c4e9e1163998c8d
Ove Eriksson <tobby.eriksson@gmail.com>
2014-08-21 12:59:01 +0200

    Create file

---
+++ default.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * package     Your module
+ * copyright   Copyright (C) 2014 Joomla Doe
+ * license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die( 'Restricted access' );
+?>
+<h3>From your first module</h3>
+<p>This is the content</p>
